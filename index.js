2a79bdbbe299ecaaff3c607e766a7fa8744ae1d2
jaseel-koolath <112375897+jaseel-koolath@users.noreply.github.com>
2023-08-03 17:58:09 +0530

    completed the exercise

--- index.js
+++ index.js
@@ -1,14 +1,28 @@
 var express = require('express');
-var cors = require('cors');
-require('dotenv').config()
+const multer = require("multer");
+var cors = require("cors");
+require("dotenv").config();
 
+const upload = multer({ dest: "uploads/" });
 var app = express();
 
 app.use(cors());
-app.use('/public', express.static(process.cwd() + '/public'));
+app.use("/public", express.static(process.cwd() + "/public"));
 
-app.get('/', function (req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
+app.get("/", function (req, res) {
+  res.sendFile(process.cwd() + "/views/index.html");
+});
+
+app.post("/api/fileanalyse", upload.single("upfile"), function (req, res) {
+  // req.file is the name of your file in the form above, here 'uploaded_file'
+  // req.body will hold the text fields, if there were any
+  const fileInfo = {
+    name: req.file.originalname,
+    type: req.file.mimetype,
+    size: req.file.size,
+  };
+  res.json(fileInfo);
+  console.log(req.file, req.body);
 });
 
 
